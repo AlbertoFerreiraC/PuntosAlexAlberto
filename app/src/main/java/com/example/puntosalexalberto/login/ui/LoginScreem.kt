89000f0b3ffba03d84b33c0348e9d4bbc9938624
Alberto Ferreira <falberto@alexsa.com.py>
2023-06-26 10:28:41 -0400

    navController para screem's

--- app/src/main/java/com/example/puntosalexalberto/login/ui/LoginScreem.kt
+++ app/src/main/java/com/example/puntosalexalberto/login/ui/LoginScreem.kt
@@ -34,18 +34,12 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.puntosalexalberto.R
-import com.example.puntosalexalberto.referenciados.ui.ReferenciadosActivity
-import com.example.puntosalexalberto.referenciados.ui.ReferenciadosScreem
 
-@Preview
 @Composable
 fun LoginScreem(navController: NavController) {
     MaterialTheme {
